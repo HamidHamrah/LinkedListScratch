83cf408ef7fb728f247d7183cd033503f55565e3
hamidhamrah <s362052@oslomet.no>
2022-10-26 16:37:57 +0200

    Har laget Delet og methoden om linkedlist innholder et vis element.

--- src/DlinkedList.java
+++ src/DlinkedList.java
@@ -1,25 +1,40 @@
-import java.util.Scanner;
+import java.util.IllegalFormatCodePointException;
 
 public class DlinkedList {
+    private int size;
+
+    public DlinkedList() {
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
     public class Node{
         int value;
         Node next;
         Node prev;
 
+
         public Node(int val) {
             this.value = val;
         }
 
-        public Node(int val, Node next, Node prev) {
+        public Node(int val, Node next, Node prev, int size) {
             this.value = val;
             this.next = next;
             this.prev = prev;
+        }
+
+        public int getSize() {
+            return size;
         }
     }
     Node head;
     Node tail;
 
-
+// Here we are going insert a node in the as a first element in the linkedlist.
     public void insertFørst(int value){
         Node current=new Node(value);
         current.next=head;
@@ -28,7 +43,10 @@ public class DlinkedList {
             head.prev=current;
         }
         head=current;
+        size++;
+
     }
+    // Here we are going Insert a node in as a last element in the linkedlist.
     public void insertLast(int value){
         Node current=new Node(value);
        if (head==null){
@@ -43,7 +61,51 @@ public class DlinkedList {
            tail=current;
            tail.next=null;
        }
+       size++;
 
+    }
+
+    // Here we are going to inesrt an element at a spesfic indeks
+    // The way it works is the that we will be give a number an we will try to insert it in the middel of an element.
+    public void insertIndeks(int after, int value){
+        Node p=findNode(after);
+        if (p==null){
+            System.out.println("Den eksister ikke");
+            return;
+        }
+        Node node=new Node(value);
+        node.next=p.next;
+        node.prev=p;
+        p.next=node;
+        if (node.next!=null){
+        node.next.prev=node;
+        }
+    }
+    public Node findNode(int after){
+        Node current=head;
+        while (current!=null){
+            if (current.value==after){
+                return current;
+            }
+            current=current.next;
+        }
+        return null;
+    }
+
+    public boolean Delet(int number){
+        Node p=findNode(number);
+        if (p.prev==null){
+            p.next.prev=null;
+            return true;
+        }
+        if (p.next==null){
+            p.prev.next=null;
+            return true;
+        }else{
+            p.prev.next=p.next;
+            p.next.prev=p.prev;
+            return true;
+        }
     }
     public void Print(){
         Node current=head;
@@ -59,6 +121,28 @@ public class DlinkedList {
             last=last.prev;
         }
         System.out.print("End");
+    }
+    public int finIndeks(int val){
+        Node node=head;
+        int indeks=0;
+        while (node!=null){
+            if (node.value==val){
+                return indeks;
+            }
+            indeks++;
+            node=node.next;
+        }
+        return -1;
+    }
+    public boolean innholder(int value){
+        Node node=head;
+        while (node!=null){
+            if (node.value==value){
+                return true;
+            }
+            node=node.next;
+        }
+        return false;
     }
 
  /*
--- src/main.java
+++ src/main.java
@@ -2,12 +2,17 @@ public class main {
     public static void main(String[] args) {
        DlinkedList list2=new DlinkedList();
 
+       
         list2.insertLast(6);
         list2.insertLast(5);
         list2.insertLast(4);
-        list2.insertFørst(7);
+        list2.insertLast(3);
+        list2.insertLast(2);
+        list2.insertLast(1);
+        list2.insertLast(0);
 
 
+        System.out.println(list2.Delet(6));
         list2.Print();
     }
 }
